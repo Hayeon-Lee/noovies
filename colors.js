ac73e4e844032ac58c8bcc05eac3968d14e188ec
Hayeon-Lee <papepopepe@naver.com>
2023-10-10 18:38:47 +0900

    feat: Dark Mode

---
+++ colors.js
@@ -0,0 +1,3 @@
+export const YELLOW_COLOR = "#ffb142";
+export const BLACK_COLOR = "#2c2c54";
+export const WHITE_COLOR = "#ffffff";
--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,53 +1,34 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { View, Text } from "react";
+import { useColorScheme } from "react-native";
 import Search from "../screens/Search";
 import Movies from "../screens/Movies";
 import Tv from "../screens/TV";
+import { YELLOW_COLOR, BLACK_COLOR, WHITE_COLOR } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-  <Tab.Navigator
-    screenOptions={{
-      tabBarLabelPosition: "beside-icon",
-      tabBarActiveTintColor: "#f6ea8c",
-      tabBarInactiveTintColor: "#F8FAFF",
-      tabBarStyle: { backgroundColor: "#353866" },
-      headerStyle: { backgroundColor: "#F8FAFF" },
-    }}
-  >
-    <Tab.Screen
-      name="Movies"
-      component={Movies}
-      options={{
-        headerTitleStyle: {
-          color: "#353866",
+const Tabs = () => {
+  const isDark = useColorScheme() === "dark";
+
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR,
         },
+        tabBarActiveTintColor: YELLOW_COLOR,
+        tabBarInactiveTintColor: isDark ? WHITE_COLOR : BLACK_COLOR,
+        headerStyle: {
+          backgroundColor: isDark ? BLACK_COLOR : WHITE_COLOR,
+        },
+        headerTintColor: isDark ? YELLOW_COLOR : BLACK_COLOR,
       }}
-    />
-    <Tab.Screen
-      name="Tv"
-      component={Tv}
-      options={{
-        tabBarBadge: "💖",
-        tabBarBadgeStyle: {
-          backgroundColor: "white",
-        },
-        headerTitleStyle: {
-          color: "#353866",
-        },
-      }}
-    />
-    <Tab.Screen
-      name="Search"
-      component={Search}
-      options={{
-        headerTitleStyle: {
-          color: "#353866",
-        },
-      }}
-    />
-  </Tab.Navigator>
-);
+    >
+      <Tab.Screen name="Movies" component={Movies} />
+      <Tab.Screen name="Tv" component={Tv} />
+      <Tab.Screen name="Search" component={Search} />
+    </Tab.Navigator>
+  );
+};
 
 export default Tabs;
