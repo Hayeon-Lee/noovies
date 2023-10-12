43f6f252e14e372aaf38f4af39e0d8aa1d3b8745
Hayeon-Lee <papepopepe@naver.com>
2023-10-12 18:43:01 +0900

    feat: Install Styled Component and Use it

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
+import styled from "styled-components/native";
+import { BLACK_COLOR } from "../colors";
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  color: ${(props) => (props.selected ? BLACK_COLOR : "yellow")};
+`;
 
 const Movies = ({ navigation: { navigate } }) => {
   return (
-    <TouchableOpacity
+    <Btn
       onPress={() => navigate("Stack", { screen: "Three" })}
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <Text>Movies</Text>
-    </TouchableOpacity>
+      <Title selected={false}>Movies</Title>
+      <Title selected={true}>Movies</Title>
+    </Btn>
   );
 };
 
