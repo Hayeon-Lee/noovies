a9099324c34986fc25c03761edca7a29dd1c1e0e
Hayeon-Lee <papepopepe@naver.com>
2023-10-14 22:11:23 +0900

    feat: Get movie lists and background image by using API

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import { ActivityIndicator, Dimensions } from "react-native";
 import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
+import { makeImgPath } from "../utils";
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
@@ -19,9 +20,15 @@ const Loader = styled.View`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
+const BgImg = styled.Image`
+  flex: 1;
+`;
+
+const Title = styled.Text``;
+
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.API_KEY;
 
 const options = {
   method: "GET",
@@ -35,16 +42,17 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
   navigation,
 }) => {
   const [loading, setLoading] = useState(true);
-  console.log(API_KEY);
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
 
   const getNowPlaying = async () => {
-    const data = await (
+    const { results } = await (
       await fetch(
         "https://api.themoviedb.org/3/movie/now_playing?language=ko-KR&page=1",
         options
       )
     ).json();
-    console.log(data);
+    setNowPlayingMovies(results);
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -63,10 +71,14 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
         controlsEnabled={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
-        <View style={{ backgroundColor: "yellow" }}></View>
-        <View style={{ backgroundColor: "pink" }}></View>
+        {nowPlayingMovies.map((movie) => (
+          <View key={movie.id}>
+            <BgImg source={{ uri: makeImgPath(movie.backdrop_path) }} />
+            {/*<BlurView>
+              <Title>{movie.original_title}</Title>
+        </BlurView>*/}
+          </View>
+        ))}
       </Swiper>
     </Container>
   );
