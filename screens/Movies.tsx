84addf2f5d11a78beed862cc9d2870c6403c40b3
Hayeon-Lee <papepopepe@naver.com>
2023-10-14 19:15:02 +0900

    feat: Use TMDB API key and modify config files

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,25 +1,57 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
+import { Dimensions } from "react-native";
+import Swiper from "react-native-web-swiper";
 import styled from "styled-components/native";
 
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
+const View = styled.View`
+  flex: 1;
 `;
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
   navigation,
 }) => {
+  console.log(API_KEY);
+  const getNowPlaying = () => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&region=KR",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
+
   return (
-    <Btn onPress={() => navigation.navigate("Stack", { screen: "Three" })}>
-      <Title>Movies</Title>
-    </Btn>
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "yellow" }}></View>
+        <View style={{ backgroundColor: "pink" }}></View>
+      </Swiper>
+    </Container>
   );
 };
 
